c68a705df0ceef3fd143d87538dc35ade9935fca
ShinWoon <brandy.hwang@gmail.com>
2021-10-26 17:06:23 +0900

    edited homepage newline and editing emotion manage part

--- peep/lib/home/client.dart
+++ peep/lib/home/client.dart
@@ -11,7 +11,6 @@ import '../sub/home_page.dart';
 import 'saveEmotion.dart';
 import 'package:peep/sub/home_page.dart';
 import 'saveEmotion.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
--- peep/lib/sub/home_page.dart
+++ peep/lib/sub/home_page.dart
@@ -8,6 +8,7 @@ import 'package:peep/player/music_player_page.dart';
 import 'package:peep/player/player_controller.dart';
 import '../home/client.dart';
 import '../home/emotion_chart.dart';
+import '../home/emotion_manage.dart';
 
 //홈페이지
 
@@ -17,12 +18,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   int blueFreq;
   int fearFreq;
   int angryFreq;
   int happyFreq;
   int calmFreq;
+
 
   @override
   void initState() {
@@ -45,6 +47,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    EmotionManger emotionManger = new EmotionManger();
+
     final user = FirebaseAuth.instance.currentUser;
     var name, photo, email, uid;
     if (user != null) {
@@ -58,221 +63,204 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
           child: Container(
               child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: 16.0,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0),
-            child: Text(
-              //회원정보 가져와서 회원 이름 + 님의 감정 분석
-              name + ' 님의 감정이 기록되었습니다',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Center(
-              child: StreamBuilder<QuerySnapshot>(
-                  stream: firestore.collection("emotion").snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return CircularProgressIndicator();
-                    } else {
-                      blueFreq = snapshot.data.docs[0]['sad'];
-                      happyFreq = snapshot.data.docs[0]['happy'];
-                      angryFreq = snapshot.data.docs[0]['angry'];
-                      calmFreq = snapshot.data.docs[0]['calm'];
-                      fearFreq = snapshot.data.docs[0]['fear'];
-                      return EmotionChart(
-                        chartData: [
-                          ChartData('BLUE', blueFreq),
-                          ChartData('FEAR', fearFreq),
-                          ChartData('ANGRY', angryFreq),
-                          ChartData('HAPPY', happyFreq),
-                          ChartData('CALM', calmFreq)
-                        ],
-                      );
-                    }
-                  })),
-          SizedBox(
-            height: 16.0,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0),
-            child: Text(
-              '오늘의 감정 키워드',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 5, bottom: 0, top: 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      ///회원정보 가져와서 회원 이름 + 님의 감정 분석
+                      name + ' 님의 감정이 기록되었습니다',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Center(
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: firestore.collection("emotion").snapshots(),
+                      builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return CircularProgressIndicator();
+                      } else {
+                        blueFreq = snapshot.data.docs[0]['sad'];
+                        happyFreq = snapshot.data.docs[0]['happy'];
+                        angryFreq = snapshot.data.docs[0]['angry'];
+                        calmFreq = snapshot.data.docs[0]['calm'];
+                        fearFreq = snapshot.data.docs[0]['fear'];
+                        return EmotionChart(
+                          chartData: [
+                            ChartData('BLUE', blueFreq),
+                            ChartData('FEAR', fearFreq),
+                            ChartData('ANGRY', angryFreq),
+                            ChartData('HAPPY', happyFreq),
+                            ChartData('CALM', calmFreq)
+                          ],
+                        );
+                      }
+                    })),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      '오늘의 감정 키워드',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 SizedBox(
-                    width: 191,
-                    height: 124.54,
-                    child: IconButton(
-                      icon: SvgPicture.asset(
-                        'assets/itd/ITD_button_HAPPY.svg',
+                  height: 8.0,
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 5, bottom: 0, top: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
                         width: 191,
                         height: 124.54,
-                        fit: BoxFit.fill,
-                      ),
-                      onPressed: () async {
-                        await setData("happy");
-                        await ClientTest().readAndWrite("happy");
-                        AudioManager.emotion = "happy";
-                        Navigator.push(
+                        child: IconButton(
+                          icon: SvgPicture.asset(
+                            'assets/itd/ITD_button_HAPPY.svg',
+                            width: 191,
+                            height: 124.54,
+                            fit: BoxFit.fill,
+                          ),
+                          onPressed: () async {
+                            await emotionManger.readWriteEmotion("happy");
+                            await ClientTest().readAndWrite("happy");
+                            AudioManager.emotion = "happy";
+                            Navigator.push(
+                              //getImage(ImageSource.camera);
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MusicPlayerPage()));
+                            },
+                        )),
+                    SizedBox(width: 0),
+                    SizedBox(
+                      width: 191,
+                      height: 124.54,
+                      child: IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/itd/ITD_button_BLUE.svg',
+                          width: 191,
+                          height: 124.54,
+                          fit: BoxFit.fill,
+                        ),
+                        onPressed: () async {
+                          await emotionManger.readWriteEmotion("sad");
+                          await ClientTest().readAndWrite("sad");
+                          AudioManager.emotion = "sad";
+                          Navigator.push(
+                              //getImage(ImageSource.camera);
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MusicPlayerPage()));
+                        },
+                      )),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 5, bottom: 0),
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 191,
+                      height: 124.54,
+                      child: IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/itd/ITD_button_ANGRY.svg',
+                           width: 191,
+                          height: 124.54,
+                          fit: BoxFit.fill,
+                        ),
+                        onPressed: () async {
+                          await emotionManger.readWriteEmotion("angry");
+                          await ClientTest().readAndWrite("angry");
+                          AudioManager.emotion = "angry";
+                          Navigator.push(
                             //getImage(ImageSource.camera);
                             context,
                             MaterialPageRoute(
                                 builder: (context) => MusicPlayerPage()));
-                      },
-                    )),
-                SizedBox(width: 0),
-                SizedBox(
-                    width: 191,
-                    height: 124.54,
-                    child: IconButton(
-                      icon: SvgPicture.asset(
-                        'assets/itd/ITD_button_BLUE.svg',
-                        width: 191,
-                        height: 124.54,
-                        fit: BoxFit.fill,
-                      ),
-                      onPressed: () async {
-                        await setData("sad");
-                        await ClientTest().readAndWrite("sad");
-                        AudioManager.emotion = "sad";
-                        Navigator.push(
+                        },
+                      )),
+                    SizedBox(
+                    width: 0,
+                    ),
+                    SizedBox(
+                      width: 191,
+                      height: 124.54,
+                      child: IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/itd/ITD_button_CALM.svg',
+                          width: 191,
+                          height: 124.54,
+                          fit: BoxFit.fill,
+                        ),
+                        onPressed: () async {
+                          await emotionManger.readWriteEmotion("calm");
+                          await ClientTest().readAndWrite("calm");
+                          AudioManager.emotion = "calm";
+                          Navigator.push(
                             //getImage(ImageSource.camera);
                             context,
                             MaterialPageRoute(
                                 builder: (context) => MusicPlayerPage()));
-                      },
-                    )),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 5, bottom: 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                    width: 191,
-                    height: 124.54,
-                    child: IconButton(
-                      icon: SvgPicture.asset(
-                        'assets/itd/ITD_button_ANGRY.svg',
-                        width: 191,
-                        height: 124.54,
-                        fit: BoxFit.fill,
-                      ),
-                      onPressed: () async {
-                        await setData("angry");
-                        await ClientTest().readAndWrite("angry");
-                        AudioManager.emotion = "angry";
-                        Navigator.push(
+                        },
+                      )),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 5),
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 191,
+                      height: 124.54,
+                      child: IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/itd/ITD_button_FEAR.svg',
+                          width: 191,
+                          height: 124.54,
+                          fit: BoxFit.fill,
+                        ),
+                        onPressed: () async {
+                          await emotionManger.readWriteEmotion("fear");
+                          await ClientTest().readAndWrite("fear");
+                          AudioManager.emotion = "fear";
+                          Navigator.push(
                             //getImage(ImageSource.camera);
                             context,
                             MaterialPageRoute(
                                 builder: (context) => MusicPlayerPage()));
-                      },
-                    )),
-                SizedBox(
-                  width: 0,
+                        },
+                      )),
+                    ],
+                  ),
                 ),
                 SizedBox(
-                    width: 191,
-                    height: 124.54,
-                    child: IconButton(
-                      icon: SvgPicture.asset(
-                        'assets/itd/ITD_button_CALM.svg',
-                        width: 191,
-                        height: 124.54,
-                        fit: BoxFit.fill,
-                      ),
-                      onPressed: () async {
-                        await setData("calm");
-                        await ClientTest().readAndWrite("calm");
-                        AudioManager.emotion = "calm";
-                        Navigator.push(
-                            //getImage(ImageSource.camera);
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => MusicPlayerPage()));
-                      },
-                    )),
+                  height: 24.0,
+                ),
               ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                    width: 191,
-                    height: 124.54,
-                    child: IconButton(
-                      icon: SvgPicture.asset(
-                        'assets/itd/ITD_button_FEAR.svg',
-                        width: 191,
-                        height: 124.54,
-                        fit: BoxFit.fill,
-                      ),
-                      onPressed: () async {
-                        await setData("fear");
-                        await ClientTest().readAndWrite("fear");
-                        AudioManager.emotion = "fear";
-                        Navigator.push(
-                            //getImage(ImageSource.camera);
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => MusicPlayerPage()));
-                      },
-                    )),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 24.0,
-          ),
-        ],
-      ))),
+          ))),
     );
-  }
-
-  Future<void> setData(String emotionState) async {
-    var uid;
-    final user = FirebaseAuth.instance.currentUser;
-    final databaseReference = FirebaseDatabase.instance.reference().child("emotion");
-
-    if(user!=null){
-      uid=user.uid;
-    }
-    await databaseReference.child(uid +"/"+ "current").update({
-      "emotion" : emotionState
-    });
-  //   await firestore
-  //       .collection("current")
-  //       .doc("state")
-  //       .update({"emotion": emotionState});
   }
 }
