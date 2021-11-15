cb30f6f27e5e7e9d714c1aec3c5574f06e5f0113
seul <dew20330@gmail.com>
2021-11-16 01:24:10 +0900

    add recentplaylist

--- peep/lib/sub/user_page.dart
+++ peep/lib/sub/user_page.dart
@@ -1,6 +1,9 @@
-import 'dart:ffi';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:peep/login/user_manager.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import '../db_manager.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:peep/home/emotion_detection.dart';
@@ -8,6 +11,9 @@ import 'package:peep/login/google_sign_in.dart';
 import 'package:peep/player/music_player_page.dart';
 import 'package:provider/provider.dart';
 import 'package:shake/shake.dart';
+import 'package:peep/player/audio_manager.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 
 class UserPage extends StatefulWidget {
   @override
@@ -224,39 +230,402 @@ class _UserPageState extends State<UserPage> {
                 SizedBox(
                 height: 10.0,
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    color: Color(0xFFF3F4F6),
+                // SizedBox(
+                //   width: double.infinity,
+                //   height: 50,
+                //   child: Container(
+                //     alignment: Alignment.centerLeft,
+                //     color: Color(0xFFF3F4F6),
+                //     child: Text(
+                //       '   좋아요 한 곡',
+                //       style: TextStyle(
+                //           color: Colors.black,
+                //           fontSize: 15,
+                //           fontWeight: FontWeight.bold),
+                //     ),
+                    
+                //   ),
+                  
+
+                // ),
+                Container(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xfff6f7f9),
+                      minimumSize: Size.fromHeight(45),
+                      shadowColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => likeSongs()),
+                      );
+                    },
+                    
                     child: Text(
-                      '   좋아요 한 곡',
+                      '좋아요 한 곡',
                       style: TextStyle(
                           color: Colors.black,
                           fontSize: 15,
                           fontWeight: FontWeight.bold),
                     ),
                   ),
-
                 ),
-              SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    color: Color(0xFFFFFFFF),
+                SizedBox(
+                height: 10.0,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: ElevatedButton(
+                    
+                    style: ElevatedButton.styleFrom(
+                      
+                      primary: Color(0xfff6f7f9),
+                      minimumSize: Size.fromHeight(45),
+                      shadowColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => recentPlayList()),
+                      );
+                    },
+                    
                     child: Text(
-                      '   최근에 들은 곡',
+                      '최근에 들은 곡',
+                      textAlign: TextAlign.start,
                       style: TextStyle(
+                          
                           color: Colors.black,
                           fontSize: 15,
                           fontWeight: FontWeight.bold),
+                          
                     ),
-                  )
+                  ),
+                ),
+              SizedBox(
+                height: 35.0,
               ),
               ],
             )),
           ));
   }
 }
+
+
+
+final userFavoriteRef = DBManager.instance.ref
+      .child("favorite")
+      .child(UserManager.instance.user.uid);
+
+final user = FirebaseAuth.instance.currentUser;
+var uid = user.uid;
+
+final songsFavoriteRef = DBManager.instance.ref.child("songs");
+DatabaseReference listLike = new FirebaseDatabase().reference();
+
+final db = FirebaseDatabase.instance.reference().child("favorite").child(uid);
+
+final ref = FirebaseDatabase.instance.reference();
+
+class likeSongs extends StatefulWidget{
+  _likeSongState createState() => _likeSongState();
+}
+
+class _likeSongState extends State<likeSongs>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        title: Text(
+          '좋아요 한 곡',
+        ),),
+        body: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 20.0
+          ),
+          child: StreamBuilder(
+            stream: listLike.child("favorite").child(uid).onValue,
+            builder: (context, AsyncSnapshot<Event> snap){
+                if(!snap.hasData) return Text("loading");
+               
+                return Column(
+                  children: <Widget>[
+                    //Text(snap.data.snapshot.value.runtimeType.toString()),
+                    
+                    Text(snap.data.snapshot.value.toString()),
+                    Text(snap.data.snapshot.value[1].toString()),
+                    //controlSearching('-MnYGbGO4FDnYZIzp7qe'),
+                      
+                  ],
+                );
+              },
+            )
+          ),
+        );
+  }
+}
+
+class LikeSongs extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<LikeSongs> {
+  final fb = FirebaseDatabase.instance;
+  
+  String _searchText = "";
+
+  _SearchScreenState() {
+    
+  }
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.reference();
+    controlSearching("-MnYFlz3HZWuz10dTQD4");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        
+      ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+          
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              controlSearching("-MnYFlz3HZWuz10dTQD4"),
+              futureSearchResults == null
+                  ? displayNoSearchResultScreen()
+                  : diplaySearchResultScreen(),
+            ]),
+      )),
+    );
+  }
+}
+
+displayNoSearchResultScreen() {
+  return Container(
+      child: Center(
+    child: ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        Text(
+          '좋아요 한 곡이 없습니다.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
+        )
+      ],
+    ),
+  ));
+}
+
+diplaySearchResultScreen() {
+  return SongResult();
+}
+
+class SongResult extends StatefulWidget {
+  @override
+  _SongResultState createState() => _SongResultState();
+}
+
+class _SongResultState extends State<SongResult> {
+  bool _favoriteSong = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(3),
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: (
+                  Image.network(GetSongCover(futureSearchResults))),
+
+
+                title: Text(
+                  GetSongTitle(futureSearchResults),
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  GetSongArtist(futureSearchResults),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                  ),
+                ),
+                
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+
+
+class SongInfo {
+  String artist;
+  //List<String> emotions;
+  //List<String> genre;
+  //List<String> tags;
+  String title;
+  //int year;
+
+  SongInfo(String artist, String title) {
+    this.artist = artist;
+    this.title = title;
+  }
+
+  String getTitle() {
+    return this.title;
+  }
+
+  String getArtist() {
+    return this.artist;
+  }
+}
+
+GetSongTitle(String sinfo) {
+  String songTitle;
+  String info;
+  info = sinfo;
+
+  String token = ', tags:';
+  String token2 = 'title: ';
+  int lastIndex = info.indexOf(token);
+  int startIndex = info.indexOf(token2);
+  songTitle = info.substring(startIndex + 7, lastIndex);
+  return songTitle;
+}
+
+GetSongArtist(String sinfo) {
+  String songArtist;
+  String info;
+  info = sinfo;
+
+  String token = ', year:';
+  String token2 = 'artist: ';
+  int lastIndex = info.indexOf(token);
+  int startIndex = info.indexOf(token2);
+  songArtist = info.substring(startIndex + 8, lastIndex);
+  return songArtist;
+}
+
+GetSongCover(String sinfo){
+  String songCover;
+  String info;
+  info = sinfo;
+
+  String token = ', title';
+  String token2 = 'artwork: ';
+  int lastIndex = info.indexOf(token);
+  int startIndex = info.indexOf(token2);
+  songCover = info.substring(startIndex + 9, lastIndex);
+  return songCover;
+}
+
+
+String futureSearchResults;
+controlSearching(str) {
+  String _searchText = '-MnYFlz3HZWuz10dTQD4';
+  final ref = FirebaseDatabase.instance.reference();
+
+  ref
+      .child("songs")
+      .equalTo("-MnYFlz3HZWuz10dTQD4")
+      .once()
+      .then((DataSnapshot data) {
+    if (data != null) {
+      print(data.value);
+      print(GetSongTitle(data.value.toString()));
+      print(GetSongArtist(data.value.toString()));
+      futureSearchResults = data.value.toString();
+    } else
+      print('일치하는 곡이 없습니다.');
+  });
+}
+
+// 최근에 들은 곡
+class recentPlayList extends StatefulWidget{
+  _recentPlayList createState() => _recentPlayList();
+}
+
+class _recentPlayList extends State<recentPlayList>{
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        title: Text(
+          '최근에 들은 곡',
+        ),),
+      body: Container(
+        
+        height: 480.0,
+        child: StreamBuilder<SequenceState>(
+          stream: AudioManager.instance.player.sequenceStateStream,
+          builder: (context, snapshot) {
+            //final user = FirebaseAuth.instance.currentUser;
+            //var uid = user.uid;
+
+            //final db = FirebaseDatabase.instance.reference().child("favorite").child(uid);
+            final state = snapshot.data;
+            final sequence = state?.sequence ?? [];
+            // void findLikes(){
+            //     db.once().then((DataSnapshot snapshot){
+            //     Map<dynamic, dynamic> values = snapshot.value;
+            //       values.forEach((key,values) {
+            //         print(values["like"]);
+            //       });
+            //   });
+            //   }
+
+            return ReorderableListView(
+              onReorder: (int oldIndex, int newIndex) {
+                if (oldIndex < newIndex) newIndex--;
+                AudioManager.instance.playlist.move(oldIndex, newIndex);
+              },
+              children: [
+                 for (var i = 0; i < sequence.length; i++)
+                    Material(
+                      key: ValueKey(sequence[i]),
+                      color: i == state.currentIndex
+                          ? Colors.grey.shade300
+                          : null,
+                      child: ListTile(
+                        title: Text(sequence[i].tag.title as String),
+                        subtitle: Text(sequence[i].tag.artist as String),
+                        
+                        onTap: () {
+                          AudioManager.instance.player.seek(Duration.zero, index: i);
+                        },
+                      ),
+                    ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+}
+
+
+
+
